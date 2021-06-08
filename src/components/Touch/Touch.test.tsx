6c8ffd6dfb42a194bd082930fa969abb97028149
Vladimir Klepov <v.klepov@gmail.com>
2021-06-08 19:25:44 +0300

    fix: Prevent firing click event on gallery content after slide

--- src/components/Touch/Touch.test.tsx
+++ src/components/Touch/Touch.test.tsx
@@ -48,7 +48,7 @@ describe('Touch', () => {
       slideRight(screen.getByTestId('inner'));
       expect(clicked.container).toBe(false);
       expect(clicked.touch).toBe(false);
-      // expect(clicked.content).toBe(false);
+      expect(clicked.content).toBe(false);
     });
   });
 });
